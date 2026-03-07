f5857aa55cd749bcfde8667f302c5a87158bc20d
Jefferson <jefferson.iskandar@gmail.com>
2026-03-07 19:08:14 +0700

    update database.py

--- database.py
+++ database.py
@@ -6,7 +6,8 @@ Database_URL = "sqlite:///db/restaurant.db"
 engine = create_engine(
     Database_URL,
     echo=True, 
-    future=True
+    future=True,
+    connect_arg={"check_same_thread": False}
 )
 
 # Init all tables
