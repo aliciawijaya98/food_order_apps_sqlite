ee3a482a477775036badb613aaf3a3c6978d6a5b
Jefferson <jefferson.iskandar@gmail.com>
2026-03-07 18:27:01 +0700

    Fix database connection for every user

--- database.py
+++ database.py
@@ -1,13 +1,6 @@
 from sqlalchemy import create_engine
 
-
-db_user = "root"
-db_pass = "admin"
-db_host = "127.0.0.1"
-db_port = 3306
-db_name = "restaurant"
-        
-Database_URL = "sqlite:///C:/sqlite_db/restaurant.db"
+Database_URL = "sqlite:///db/restaurant.db"
                 
 # engine untuk database restaurant
 engine = create_engine(
@@ -23,9 +16,11 @@ def init_all_tables():
     from order_database_sqlite import init_order_tables
 
     print("Initializing database...")
+    
     init_users_table()
     init_menu_table()
     init_order_tables()
+    
     print("All tables created successfully!")
 
 if __name__ == "__main__":
