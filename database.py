361faa08ddb25bc6fd134c8cf4e5870615704538
Alicia Wijaya <alicia.wijaya@hotmail.com>
2026-03-17 14:43:36 +0700

    Convert to ORM

--- database.py
+++ database.py
@@ -1,28 +1,18 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-Database_URL = "sqlite:///db/restaurant.db"
+DATABASE_URL = "sqlite:///db/restaurant.db"
                 
 # engine untuk database restaurant
 engine = create_engine(
-    Database_URL,
-    echo=True, 
-    future=True,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    echo=True
 )
 
-# Init all tables
-def init_all_tables():
-    from user_database_sqlite import init_users_table
-    from menu_database_sqlite import init_menu_table
-    from order_database_sqlite import init_order_tables
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
 
-    print("Initializing database...")
-    
-    init_users_table()
-    init_menu_table()
-    init_order_tables()
-    
-    print("All tables created successfully!")
-
-if __name__ == "__main__":
-    init_all_tables()
+Base = declarative_base()
